fec555649b0d76773d9c78a944121b9e6ae489f8
razmanov666 <razmanov666@gmail.com>
2022-07-21 01:37:50 +0300

    remove testing function

--- tutorial_site/news/views.py
+++ tutorial_site/news/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
 
 def index(request):
-    # print(request.META)
-    return HttpResponse('Hello world')
-
-
-def test(request):
-    return HttpResponse('<h1>Тестовая страница!!!</h1>')
+    news = News.objects.order_by('-created_at')
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
