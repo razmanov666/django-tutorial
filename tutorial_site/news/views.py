b3b1daaa8fa01eecd8430081b9018a759c977b3f
razmanov666 <razmanov666@gmail.com>
2022-07-25 15:24:14 +0300

    create and config model Category

--- tutorial_site/news/views.py
+++ tutorial_site/news/views.py
@@ -1,8 +1,11 @@
+# from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from .models import News
 
+
 def index(request):
-    news = News.objects.order_by('-created_at')
-    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
+    news = News.objects.all()
+    return render(
+        request, "news/index.html", {"news": news, "title": "Список новостей"}
+    )
