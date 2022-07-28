51f28946ba4e85d7b56166baeccf9bfbb6e228b8
razmanov666 <razmanov666@gmail.com>
2022-07-28 17:57:51 +0300

    add path for view_news

--- tutorial_site/news/urls.py
+++ tutorial_site/news/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import get_category
 from .views import index
+from .views import view_news
 
 urlpatterns = [
     path("", index, name="home"),
     path("category/<int:category_id>/", get_category, name="category"),
+    path("news/<int:news_id>/", view_news, name="view_news"),
 ]
