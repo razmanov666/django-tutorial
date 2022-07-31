8ca6e8da40676d6bd1ca502f3c7eccec091f1e0f
Alexey Razmanov <razmanov666@gmail.com>
2022-07-31 17:32:16 +0300

    use simple method for creating forms

--- tutorial_site/news/forms.py
+++ tutorial_site/news/forms.py
@@ -1,23 +1,20 @@
 from django import forms
 
-from .models import Category
+from .models import News
 
 
-class NewsForm(forms.Form):
-    title = forms.CharField(
-        max_length=150,
-        label="Заголовок",
-        widget=forms.TextInput(attrs={"class": "form-control"}),
-    )
-    content = forms.CharField(
-        label="Содержимое",
-        required=False,
-        widget=forms.Textarea(attrs={"class": "form-control"}),
-    )
-    category = forms.ModelChoiceField(
-        queryset=Category.objects.all(),
-        label="Категория",
-        empty_label="Выберите категорию",
-        widget=forms.Select(attrs={"class": "form-control", "row": 5}),
-    )
-    is_published = forms.BooleanField(label="Опубликовать?", initial=True)
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+
+        # Auto creating all fields
+        # fields = '__all__'
+
+        fields = ["title", "content", "category", "is_published"]
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "content": forms.Textarea(attrs={"class": "form-control"}),
+            "category": forms.Select(
+                attrs={"class": "form-control", "row": 5}
+            ),
+        }
