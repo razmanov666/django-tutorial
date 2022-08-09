98c81b9bc4a758d62bf99d61569208d51f76168c
Alexey Razmanov <razmanov666@gmail.com>
2022-08-09 19:21:37 +0300

    add filter to unpublished news

--- tutorial_site/news/templatetags/news_tags.py
+++ tutorial_site/news/templatetags/news_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.db.models import Count
+from django.db.models import F
 from news.models import Category
 
 register = template.Library()
@@ -7,7 +8,9 @@ register = template.Library()
 
 @register.simple_tag
 def get_categories():
-    return Category.objects.annotate(cnt=Count("news")).filter(cnt__gt=0)
+    return Category.objects.annotate(
+        cnt=Count("news", filter=F("news__is_published"))
+    ).filter(cnt__gt=0)
 
 
 @register.inclusion_tag("news/list_categories.html")
